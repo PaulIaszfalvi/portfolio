047676a5145e75680bac0b3dcee4997591606f79
Paul <paul.iaszfalvi@gmail.com>
2022-04-19 10:47:40 -0400

    Fixed the portfolio section, created cards, etc

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,28 +4,36 @@ import "./navbar.css";
 
 function navbar() {
   return (
-    <div className="navibar fixed-top">
+    <div className="navibar fixed-top ">
       <Navbar expand="lg">
-        <Navbar.Brand className="col-sm-2 col-xs-auto" href="/">
-          Portfolio PI
+        <Navbar.Brand className="text-white col-sm-2 col-xs-auto" href="/">
+          Portfolio Paul Iaszfalvi
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse
-          className="button-holder col-sm-10"
+          className="button-holder col </Nav.Item>-sm-10"
           id="basic-navbar-nav"
         >
-          <Nav className="ml-auto">
+          <Nav className="ml-auto ">
             <Nav.Item>
-              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link className="nav-link text-white" href="#home">
+                Home
+              </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link className="nav-link text-white" href="#about">
+                About
+              </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link className="nav-link text-white" href="#portfolio">
+                Portfolio
+              </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link className="nav-link text-white" href="#contact">
+                Contact
+              </Nav.Link>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
